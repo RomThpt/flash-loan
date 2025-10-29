b0f6104a5e5de25ae261dfae75718bc6638496d1
RomThpt <reeflk8@gmail.com>
2025-10-29 11:25:09 +0100

    Refactor import statements and improve error handling in FlashLoan contract

--- src/FlashLoan.sol
+++ src/FlashLoan.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/dependencies/openzeppelin/contracts/IERC20.sol";
 
 contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable s_owner;
@@ -12,11 +12,13 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
         s_owner = payable(msg.sender);
     }
 
-    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params)
-        external
-        override
-        returns (bool)
-    {
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address, /*initiator*/
+        bytes calldata /*params*/
+    ) external override returns (bool) {
         // we have borrowed the funds, we need to pay them back
         //logic
         uint256 amountOwed = amount + premium;
@@ -34,7 +36,8 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
 
     function withdraw(address _asset) public onlyOwner {
         uint256 amount = getBalance(_asset);
-        IERC20(_asset).transfer(s_owner, amount);
+        bool result = IERC20(_asset).transfer(s_owner, amount);
+        require(result, "Transfer failed");
     }
 
     modifier onlyOwner() {
